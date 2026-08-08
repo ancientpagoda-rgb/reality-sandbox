f3753c97e8b90489024a95a2695eb8ded2523b3e
ancientpagoda-rgb <ancientpagoda@gmail.com>
2026-08-08 14:51:28 -0500

    Load surface camera performance pass

--- core/surface-mode-entry.js
+++ core/surface-mode-entry.js
@@ -1,6 +1,7 @@
 import './pointer-lock-compat.js';
 import './surface-mode.js';
 import './surface-performance.js';
+import './surface-camera-performance.js';
 import './surface-mobile-controls.js';
 import './surface-mode-dblclick-bridge.js';
 import './presentation-invariant-compat.js';
